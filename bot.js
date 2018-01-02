b31f2bd316c790cbdc823f1ac1e25f6dd28a2850
MyLewdBot <35025989+MyLewdBot@users.noreply.github.com>
2018-01-02 09:46:35 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Set the prefix
+const prefix = "!";
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
+client.on("message", (message) => {
+  // Exit and stop if it's not there
+  if (!message.content.startsWith(prefix)) return;
 
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
+  if (message.content.startsWith(prefix + "nick")) {
+    var args = message.content.substring(PREFIX.length).split(" ");
+    message.channel.send(args[1]);
+  }
+    
 });
 
 // THIS  MUST  BE  THIS  WAY
