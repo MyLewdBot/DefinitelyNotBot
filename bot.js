8a4a471a144804c7c64011f38f117015454e0d85
MyLewdBot <35025989+MyLewdBot@users.noreply.github.com>
2018-01-02 10:12:52 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const sql = require("sqlite");
-sql.open("./nick.sqlite");
+sql.open("./nicks.sqlite");
 
 client.on('ready', () => {
     console.log('I am ready!');
